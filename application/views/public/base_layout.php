4e5228eb71b918fbefafb6fd83be6a3b47ac57ec
agungwahyu23 <52806335+agungwahyu23@users.noreply.github.com>
2023-05-01 16:06:26 +0700

    font size

--- application/views/public/base_layout.php
+++ application/views/public/base_layout.php
@@ -487,11 +487,11 @@
                 <div class="col-12 col-sm-6 col-lg-4">
                     <div class="single-footer">
                         <div class="logo-area footer">
-                            <a href="#"><img src="<?= base_url() ?>assets/public/assets/img/logo-removebg-preview.png" alt=""></a>
+                            <img src="<?= base_url() ?>assets/public/assets/img/logo-removebg-preview.png" alt="">
                         </div>
                     </div>
                 </div>
-                <div class="col-6 col-sm-6 col-lg-2 mt-3">
+                <div class="col-6 col-sm-6 col-lg-2 mt-4">
                     <div class="single-footer">
                         <ul>
                             <li><a href="#">About</a></li>
@@ -510,7 +510,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col mt-4">
                     <div class="single-footer">
                         <div class="footer-form">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.2175469782387!2d112.73005171414916!3d-7.329446574117059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb423c9f28ff%3A0x30b02913187c105a!2sJl.%20Jemur%20Ngawinan%20I%20No.53%2C%20Jemur%20Wonosari%2C%20Kec.%20Wonocolo%2C%20Kota%20SBY%2C%20Jawa%20Timur%2060237!5e0!3m2!1sid!2sid!4v1680618282973!5m2!1sid!2sid" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
